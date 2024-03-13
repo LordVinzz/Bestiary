76bdd02df6ef7427a00d47640b30f538804ff1ad
Anthony LAURENT <50252147+Azel-ytof@users.noreply.github.com>
2024-03-13 16:40:22 +0000

    do not store json anymore in creature class

--- src/creature.py
+++ src/creature.py
@@ -6,7 +6,6 @@ from random import randint
 class Creature:
     bestiary = None
 
-
     @staticmethod
     def get_bestiary():
         if Creature.bestiary is None:
@@ -17,20 +16,17 @@ class Creature:
 
         return Creature.bestiary
 
-
     @staticmethod
     def calculate_skill_base(name, skill):
         bestiary = Creature.get_bestiary()
-        return bestiary[name][skill] + randint(
-            bestiary[name][skill + "_std"][0],
-            bestiary[name][skill + "_std"][1],
+        return randint(
+            bestiary[name][skill]['min'],
+            bestiary[name][skill]['max'],
         )
-
 
     @staticmethod
     def calculate_ac(base_ac, dex):
         return base_ac + (dex - 10) // 2
-
 
     @staticmethod
     def calculate_mhp(base_hp, level, con, hp_dices):
@@ -40,85 +36,44 @@ class Creature:
             + randint(hp_dices[0], (level - 1) * hp_dices[1] * hp_dices[0])
         )
 
-
-    def skill_add_pts(self, skill, n=1):
-        self.json_data[skill]["Value"] += n
-
-
-    def get_value(self, key):
-        return self.json_data[key]["Value"]
-
-
     def get_bestiary_data(self, key):
-        return self.bestiary[self.get_value("NAM")][key]
-
+        return self.bestiary[self.name][key]
 
     def __init__(self, name, level, card_id):
         self.bestiary = Creature.get_bestiary()
 
         self.card_id = card_id
-
-        self.json_data = {
-            "NAM": {"Value": name, "Immuable": True},
-            "STR": {
-                "Value": Creature.calculate_skill_base(name, "STR"),
-                "Immuable": False,
-            },
-            "DEX": {
-                "Value": Creature.calculate_skill_base(name, "DEX"),
-                "Immuable": False,
-            },
-            "CON": {
-                "Value": Creature.calculate_skill_base(name, "CON"),
-                "Immuable": False,
-            },
-            "INT": {
-                "Value": Creature.calculate_skill_base(name, "INT"),
-                "Immuable": False,
-            },
-            "WIS": {
-                "Value": Creature.calculate_skill_base(name, "WIS"),
-                "Immuable": False,
-            },
-            "CHA": {
-                "Value": Creature.calculate_skill_base(name, "CHA"),
-                "Immuable": False,
-            },
-        }
+        self.name = name
+        self.strength = Creature.calculate_skill_base(name, "strength")
+        self.dexterity = Creature.calculate_skill_base(name, "dexterity")
+        self.constitution = Creature.calculate_skill_base(name, "constitution")
+        self.intelligence = Creature.calculate_skill_base(name, "intelligence")
+        self.wisdom = Creature.calculate_skill_base(name, "wisdom")
+        self.charisma = Creature.calculate_skill_base(name, "charisma")
+        self.armor_class = Creature.calculate_ac(
+            self.get_bestiary_data("armor_class"), self.dexterity
+        )
+        self.current_health_point = self.max_health_point = Creature.calculate_mhp(
+            self.get_bestiary_data("max_health_point"),
+            level,
+            self.constitution,
+            self.get_bestiary_data("hp_dices"),
+        )
 
         points = (level // 4) * 2
 
         while points > 0:
             d = randint(1, 6)
             if d == 1:
-                self.skill_add_pts("STR")
+                self.strength += 1
             elif d == 2:
-                self.skill_add_pts("DEX")
+                self.dexterity += 1
             elif d == 3:
-                self.skill_add_pts("CON")
+                self.constitution += 1
             elif d == 4:
-                self.skill_add_pts("INT")
+                self.intelligence += 1
             elif d == 5:
-                self.skill_add_pts("WIS")
+                self.wisdom += 1
             else:
-                self.skill_add_pts("CHA")
+                self.charisma += 1
             points -= 1
-
-        self.json_data["AC"] = {
-            "Value": Creature.calculate_ac(
-                self.get_bestiary_data("AC"), self.get_value("DEX")
-            ),
-            "Immuable": False,
-        }
-
-        self.json_data["MHP"] = {
-            "Value": Creature.calculate_mhp(
-                self.get_bestiary_data("Base_Hp"),
-                level,
-                self.get_value("CON"),
-                self.get_bestiary_data("Hp_Dices"),
-            ),
-            "Immuable": False,
-        }
-
-        self.json_data["HP"] = {"Value": self.get_value("MHP"), "Immuable": False}
